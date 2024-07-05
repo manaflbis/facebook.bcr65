a287527fccd72df684c3fa8fb9eb10cd73bdd923
manaflbis <code.brocamp@gmail.com>
2024-07-05 16:07:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: -20,
+
+   
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  
+  });
